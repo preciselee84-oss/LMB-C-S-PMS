762fcd75bf69c265794d929d7b447315f78833e2
preciselee84-oss <preciselee84@gmail.com>
2026-08-26 14:32:56 +0900

    Rename chatbot similar cases heading

--- app.py
+++ app.py
@@ -20296,7 +20296,7 @@ def show_cms_chatbot():
                     int(st.session_state.get("cms_chatbot_answer_index", 0)),
                     len(saved_matches) - 1,
                 )
-                st.markdown("##### 최근 유사 고객사 사례")
+                st.markdown("##### 유사 사례")
                 for idx, match in enumerate(saved_matches[:5]):
                     row = match.get("row", {})
                     case_answer, case_detail = parse_chatbot_answer_parts(str(row.get("답변요약", "")))
